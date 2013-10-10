c10bec79142ffb4211c958091bd96295a3629a4a
Philipp Lehsten <philipp.lehsten@gmail.com>
2013-10-10 15:57:42 +0200

    Beta-Version 0.1.1
    required roles to add and modify services are not provided via the settings

--- templates/configPlugin.php
+++ templates/configPlugin.php
@@ -8,26 +8,53 @@ var_dump($settings);
 
 <? if (!$casa_active) : ?>
 <form action="<?= PluginEngine::getLink("casaadminplugin/settings") ?>" method="post">
+	
+	<fieldset>
 
+	<legend>Wie wollen Sie das CASA-PLugin nutzen?</legend>
+	<label>
+	<?= _('Modus:') ?>
+	<select required name="casaConfig[useCASA]" >
+	      <option <?php if ($settings[useCASA]) : ?>selected=true' <?php endif; ?>value="1">CASA mit Server verwenden</option>
+	      <option <?php if (!$settings[useCASA]) :?>selected=true' <?php endif; ?> value="0">CASA nur lokal verwenden</option>
+	    </select>
+	</label>
+
+	</fieldset>
 <fieldset>
 
-<legend>Wo befindet sich Ihr CASA-Server?</legend>
-
-<label>
-<?= _('CASA-Server:') ?>
-<input  type="text" name="casaConfig[server]" value="<?= htmlReady($settings['server']) ?>">
-</label>
+<legend>Wo befindet sich Ihr CASA-Server? (Nur bei Verwendung mit Server!)</legend>
 
 <label>
 <?= _('Broker:') ?>
 <input  type="text" name="casaConfig[broker]" value="<?= htmlReady($settings['broker']) ?>">
 </label>
 
+</fieldset>
+<fieldset>
+<legend>Notwendige Berechtigungen</legend>
+
 <label>
-<?= _('Modus:') ?>
-<select required name="casaConfig[useCASA]" >
-      <option <?php if ($settings[useCASA]) : ?>selected=true' <?php endif; ?>value="1">CASA mit Server verwenden</option>
-      <option <?php if (!$settings[useCASA]) :?>selected=true' <?php endif; ?> value="0">CASA nur lokal verwenden</option>
+<?= _('Rolle zum Hinzuf&uuml;gen:') ?>
+<select required name="casaConfig[addRole]" >
+      <option <?php if ($settings[addRole] == 'root') : ?>selected=true' <?php endif; ?>value="root">root</option>
+      <option <?php if ($settings[addRole] == 'admin') :?>selected=true' <?php endif; ?> value="admin">admin</option>
+      <option <?php if ($settings[addRole] == 'dozent') :?>selected=true' <?php endif; ?>value="dozent">dozent</option>
+      <option <?php if ($settings[addRole] == 'tutor') :?>selected=true' <?php endif; ?> value="tutor">tutor</option>
+      <option <?php if ($settings[addRole] == 'autor') : ?>selected=true' <?php endif; ?>value="autor">autor</option>
+      <option <?php if ($settings[addRole] == 'user') :?>selected=true' <?php endif; ?> value="user">user</option>
+    </select>
+</label>
+</br>
+<label>
+<?= _('Rolle zum Verwalten:') ?>
+<select required name="casaConfig[admRole]" >
+      <option <?php if ($settings[admRole] == 'root') : ?>selected=true' <?php endif; ?>value="root">root</option>
+      <option <?php if ($settings[admRole] == 'admin') :?>selected=true' <?php endif; ?> value="admin">admin</option>
+      <option <?php if ($settings[admRole] == 'dozent') :?>selected=true' <?php endif; ?>value="dozent">dozent</option>
+      <option <?php if ($settings[admRole] == 'tutor') :?>selected=true' <?php endif; ?> value="tutor">tutor</option>
+      <option <?php if ($settings[admRole] == 'autor') : ?>selected=true' <?php endif; ?>value="autor">autor</option>
+      <option <?php if ($settings[admRole] == 'user') :?>selected=true' <?php endif; ?> value="user">user</option>
     </select>
 </label>
 
